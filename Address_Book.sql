111ba39d5ef97216e9866157ac35e02f4217d10d
mythili annamalai <mythiliannamalai45@gmail.com>
2022-04-21 20:22:02 +0530

    create table

--- Address_Book.sql
+++ Address_Book.sql
@@ -1,2 +1,14 @@
 --UC-1 Create Data base
 create database Address_Book_Service;
+
+--UC-2 create table
+create table Address_Book(
+First_Name varchar(50),
+Last_Name varchar(50),
+Person_Address varchar(50),
+City varchar(50),
+Person_State varchar(50),
+zip int, 
+Phone_Number bigint,
+Email_Id varchar(20)
+);
